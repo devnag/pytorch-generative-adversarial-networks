26db8c9fec86817d9211d2c0a424b48b14d5a0e0
Dev Nag <devnag@gmail.com>
2021-06-30 14:44:09 -0700

    Updating for Pytorch 1.9.0.

--- gan_pytorch.py
+++ gan_pytorch.py
@@ -141,14 +141,14 @@ def train():
             #  1A: Train D on real
             d_real_data = Variable(d_sampler(d_input_size))
             d_real_decision = D(preprocess(d_real_data))
-            d_real_error = criterion(d_real_decision, Variable(torch.ones([1,1])))  # ones = true
+            d_real_error = criterion(d_real_decision, Variable(torch.ones([1])))  # ones = true
             d_real_error.backward() # compute/store gradients, but don't change params
 
             #  1B: Train D on fake
             d_gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
             d_fake_data = G(d_gen_input).detach()  # detach to avoid training G on these labels
             d_fake_decision = D(preprocess(d_fake_data.t()))
-            d_fake_error = criterion(d_fake_decision, Variable(torch.zeros([1,1])))  # zeros = fake
+            d_fake_error = criterion(d_fake_decision, Variable(torch.zeros([1])))  # zeros = fake
             d_fake_error.backward()
             d_optimizer.step()     # Only optimizes D's parameters; changes based on stored gradients from backward()
 
@@ -161,7 +161,7 @@ def train():
             gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
             g_fake_data = G(gen_input)
             dg_fake_decision = D(preprocess(g_fake_data.t()))
-            g_error = criterion(dg_fake_decision, Variable(torch.ones([1,1])))  # Train G to pretend it's genuine
+            g_error = criterion(dg_fake_decision, Variable(torch.ones([1])))  # Train G to pretend it's genuine
 
             g_error.backward()
             g_optimizer.step()  # Only optimizes G's parameters
